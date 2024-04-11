dfe148b10ea10932f94eb11296bb68db666e8c06
shashankbhosagi <shashankbhosagi0121@gmail.com>
2024-04-11 14:09:18 +0530

    🧪 Check Input function

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+function checkInput(inputID) {
+  let input = document.getElementById(inputID);
+  let value = input.value;
+  let toolTip = document.getElementById(inputID + "ToolTip");
+
+  if (!/^[0-9]+$/.test(value)) {
+    toolTip.style.display = "block";
+  } else {
+    toolTip.style.display = "none";
+  }
+}
