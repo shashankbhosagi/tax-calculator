65e5906788728ed7bc6033f2af07f3134cfbdcd0
shashankbhosagi <shashankbhosagi0121@gmail.com>
2024-04-13 01:01:41 +0530

    ✨Improve Code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document
     window.location.reload();
   });
 
-function checkInput(inputID) {
+function triggerToolTip(inputID) {
   let input = document.getElementById(inputID);
   let value = input.value;
   let toolTip = document.getElementById(inputID + "ToolTip");
@@ -21,71 +21,92 @@ function calculateOverallIncome() {
   let extraIncome = document.getElementById("extraIncome").value;
   let age = Number(document.getElementById("age-select").value);
   let totalDeductions = document.getElementById("totalDeductions").value;
+  const nonTaxableIncome = 800000;
 
-  if (
-    isSpaces(grossIncome) ||
-    isSpaces(extraIncome) ||
-    isSpaces(totalDeductions)
-  ) {
-    alert("Please enter numbers that are non-negative and not empty.");
+  if (isEmptyInput(grossIncome, extraIncome, totalDeductions)) {
+    alert("Input cannot be left empty");
     return;
   }
   grossIncome = Number(grossIncome);
   extraIncome = Number(extraIncome);
   totalDeductions = Number(totalDeductions);
 
-  if (isNaN(grossIncome) || isNaN(extraIncome) || isNaN(totalDeductions)) {
-    alert("Please enter numbers that are non-negative and not empty.");
-    return;
+  if (isNonNegativeInput(grossIncome, extraIncome, totalDeductions)) {
+    alert("Please enter numbers that are non-negative and not empty");
   }
-  if (grossIncome < 0 || extraIncome < 0 || totalDeductions < 0) {
-    alert("Please enter numbers that are non-negative and not empty.");
+
+  if (inputChecks(grossIncome, extraIncome, age, totalDeductions)) {
     return;
   }
 
-  if (age === 0) {
-    alert("Age must be selected");
-    return;
-  }
-  if (totalDeductions > grossIncome + extraIncome) {
-    alert(
-      "Deductions cannot be more the netIncome (Gross Income + Extra Income)"
-    );
-    return;
-  }
   let netIncome = grossIncome + extraIncome - totalDeductions;
-  let tax = netIncome > 800000 ? calculateTax(netIncome, age) : 0;
+  let tax =
+    netIncome > nonTaxableIncome
+      ? calculateTax(netIncome, age, nonTaxableIncome)
+      : 0;
   let overallIncome = netIncome - tax;
   overallIncome = Number(overallIncome.toFixed(2));
 
-  const threshold = 1000000000000000000000000;
-  let overallIncomeDisplay;
-  if (overallIncome > threshold) {
-    overallIncomeDisplay = overallIncome.toExponential();
-  } else {
-    overallIncomeDisplay = overallIncome.toLocaleString("en-IN", {
-      style: "currency",
-      currency: "INR",
-    });
-  }
+  const overallIncomeDisplay = formatOverallIncome(overallIncome);
 
   document.getElementById("overallIncome").textContent = overallIncomeDisplay;
 
+  showModal();
+}
+
+function showModal() {
   let myModal = new bootstrap.Modal(
     document.getElementById("overallIncomeModal")
   );
   myModal.show();
 }
 
-function calculateTax(netIncome, age) {
+function isEmptyInput(grossIncome, extraIncome, totalDeductions) {
+  if (
+    isSpaces(grossIncome) ||
+    isSpaces(extraIncome) ||
+    isSpaces(totalDeductions)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function isNonNegativeInput(grossIncome, extraIncome, totalDeductions) {
+  if (isNaN(grossIncome) || isNaN(extraIncome) || isNaN(totalDeductions)) {
+    alert("Please enter numbers that are non-negative and not empty.");
+    return true;
+  }
+  if (grossIncome < 0 || extraIncome < 0 || totalDeductions < 0) {
+    alert("Please enter numbers that are non-negative and not empty.");
+    return true;
+  }
+
+  return false;
+}
+
+function inputChecks(grossIncome, extraIncome, age, totalDeductions) {
+  if (age === 0) {
+    alert("Age must be selected");
+    return true;
+  }
+  if (totalDeductions > grossIncome + extraIncome) {
+    alert(
+      "Deductions cannot be more the netIncome (Gross Income + Extra Income)"
+    );
+    return true;
+  }
+  return false;
+}
+
+function calculateTax(netIncome, age, nonTaxableIncome) {
   switch (age) {
     case 1:
-      return 0.3 * (netIncome - 800000);
+      return 0.3 * (netIncome - nonTaxableIncome);
     case 2:
-      return 0.4 * (netIncome - 800000);
-
+      return 0.4 * (netIncome - nonTaxableIncome);
     case 3:
-      return 0.1 * (netIncome - 800000);
+      return 0.1 * (netIncome - nonTaxableIncome);
   }
 }
 
@@ -93,6 +114,20 @@ function isSpaces(str) {
   return str.match(/^ *$/) !== null;
 }
 
+//
+function formatOverallIncome(overallIncome) {
+  const threshold = 1000000000000000000000000;
+  if (overallIncome > threshold) {
+    return overallIncome.toExponential();
+  } else {
+    return overallIncome.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+    });
+  }
+}
+
+//For BootStrap ToolTip to work
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
